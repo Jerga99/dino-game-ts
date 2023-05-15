682247773d87198c8a5f61023e48354f2812bb35
Filip Jerga <jerga99@gmail.com>
2023-05-15 08:12:04 -0600

    Types and modules

--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -1,7 +1,46 @@
 
 import Phaser from "phaser";
 
+
+type Age = number;
+
+let age: Age = 100;
+
+declare module DinoGame {
+
+  namespace Rendering {
+    type Person = { name: string; }
+
+    namespace Arcade {
+      type Whatever  = {
+        a: string;
+        b: number;
+      }
+    }
+
+    namespace Matter {
+      type Whatever  = {
+        a: number;
+        b: string;
+      }
+    }
+  }
+}
+
+type MatterWhatever = DinoGame.Rendering.Matter.Whatever;
+
+type Person = {
+  name: string;
+  age: number;
+  displayAge: () => void;
+  getName: () => string;
+  welcomePerson: (w: string) => string;
+}
+
 class PlayScene extends Phaser.Scene {
+
+  person: Person;
+  whatever: MatterWhatever;
 
   get gameHeight() {
     return this.game.config.height as number;
@@ -14,6 +53,31 @@ class PlayScene extends Phaser.Scene {
   create() {
     this.createEnvironment();
     this.createPlayer();
+
+    this.whatever = {
+      a: 100,
+      b: "sakdjansdkjs"
+    }
+
+    this.person = {
+      name: "Filip",
+      age: 32,
+      displayAge() {
+        console.log(this.age);
+      },
+      getName() {
+        return this.name;
+      },
+      welcomePerson(welcome: string) {
+        return welcome;
+      }
+    };
+    
+    this.person.displayAge();
+    const name = this.person.getName();
+    console.log(name);
+    const welcome = this.person.welcomePerson("Hello");
+    console.log(welcome);
   }
 
   createPlayer() {
