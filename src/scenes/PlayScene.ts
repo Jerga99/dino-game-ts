81f68fb12eef051bada1223ce7f1eeaf8c420459
Filip Jerga <jerga99@gmail.com>
2023-05-15 07:24:10 -0600

    Game height and types

--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -2,16 +2,23 @@
 import Phaser from "phaser";
 
 class PlayScene extends Phaser.Scene {
+
+  get gameHeight() {
+    return this.game.config.height as number;
+  }
+
   constructor() {
     super("PlayScene");
   }
 
   create() {
     this.add
-      .tileSprite(0, 340, 88, 26, "ground")
+      .tileSprite(0, this.gameHeight, 88, 26, "ground")
       .setOrigin(0, 1);
   }
 
 }
 
 export default PlayScene;
+
+
