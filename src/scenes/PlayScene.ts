015b0c5030fd8a05e5f67492774b838fa2fb5ebe
Filip Jerga <jerga99@gmail.com>
2023-05-17 07:43:10 -0600

    Roll out the ground

--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -5,10 +5,15 @@ import { SpriteWithDynamicBody } from "../types";
 class PlayScene extends Phaser.Scene {
 
   player: Player;
+  ground: Phaser.GameObjects.TileSprite;
   startTrigger: SpriteWithDynamicBody;
 
   get gameHeight() {
     return this.game.config.height as number;
+  }
+
+  get gameWidth() {
+    return this.game.config.width as number;
   }
 
   constructor() {
@@ -27,12 +32,20 @@ class PlayScene extends Phaser.Scene {
 
       if (this.startTrigger.y === 10) {
         this.startTrigger.body.reset(0, this.gameHeight);
-        console.log("Triggering upper Trigger!");
         return;
       }
 
       this.startTrigger.body.reset(9999, 9999);
-      console.log("Roll out the ground and start the game!");
+      
+      this.time.addEvent({
+        delay: 1000 / 60,
+        loop: true,
+        callback: () => {
+          if (this.ground.width <= this.gameWidth) {
+            this.ground.width += (17 * 2);
+          }
+        }
+      });
     });
   }
 
@@ -41,9 +54,12 @@ class PlayScene extends Phaser.Scene {
   }
 
   createEnvironment() {
-    this.add
+    this.ground = this.add
       .tileSprite(0, this.gameHeight, 88, 26, "ground")
       .setOrigin(0, 1);
+  }
+
+  update(time: number, delta: number): void {
   }
 }
 
