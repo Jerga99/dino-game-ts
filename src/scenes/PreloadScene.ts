156821b6c292553dd31bdf6b513b84e23aad6965
Filip Jerga <jerga99@gmail.com>
2023-05-18 09:02:20 -0600

    Display Game over text

--- src/scenes/PreloadScene.ts
+++ src/scenes/PreloadScene.ts
@@ -12,6 +12,9 @@ class PreloadScene extends Phaser.Scene {
     this.load.image("dino-idle", "assets/dino-idle-2.png");
     this.load.image("dino-hurt", "assets/dino-hurt.png");
 
+    this.load.image("restart", "assets/restart.png");
+    this.load.image("game-over", "assets/game-over.png");
+
     for (let i = 0; i < PRELOAD_CONFIG.cactusesCount; i++) {
       const cactusNum = i + 1;
       this.load.image(`obstacle-${cactusNum}`, `assets/cactuses_${cactusNum}.png`);
