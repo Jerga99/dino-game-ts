6caa784a8919b527efe4836e2423ed4719f1bc51
Filip Jerga <jerga99@gmail.com>
2023-05-19 07:37:05 -0600

    Play down animation

--- src/entities/Player.ts
+++ src/entities/Player.ts
@@ -65,7 +65,9 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
   }
 
   playRunAnimation() {
-    this.play("dino-run", true);
+    this.body.height <= 58 ?
+      this.play("dino-down", true) :
+      this.play("dino-run", true);
   }
 
   registerAnimations() {
